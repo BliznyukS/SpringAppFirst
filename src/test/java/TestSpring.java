371945d4e32c3285b3dd7deca36e16995f16f404
BliznyukS <bliznyuk.sergey@gmail.com>
2020-03-30 21:20:33 +0300

    Create test directory, TestSpring move to test directory

---
+++ src/test/java/TestSpring.java
@@ -0,0 +1,19 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.alishev.springcourse.Music;
+import ru.alishev.springcourse.MusicPlayer;
+
+public class TestSpring {
+/*    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Music music = context.getBean("musicBean", Music.class);
+
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+
+        musicPlayer.playMusic();
+
+        context.close();
+    }
+
+ */
+}
